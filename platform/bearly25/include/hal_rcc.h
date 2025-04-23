86a449a46e12d374a2783518e551beca8895e701
Jasmine Angle <12690737+jasmangle@users.noreply.github.com>
2025-04-22 22:24:47 -0700

    Fix bearly25 RCC driver formatting

--- platform/bearly25/include/hal_rcc.h
+++ platform/bearly25/include/hal_rcc.h
@@ -8,42 +8,40 @@
  * 
  */
 
- #ifndef __HAL_RCC_H__
- #define __HAL_RCC_H__
- 
- #ifdef __cplusplus
- extern "C" {
- #endif
- 
- #include "metal.h"
- 
- /**
-  * \brief   Clock selection mux options.
-  * \note    Creates an enum named `ClockSel_Opts` for later use.
-  */
- typedef enum {
-   CLKSEL_SLOW = (uint32_t)0,
-   CLKSEL_PLL0 = (uint32_t)1,
-   CLKSEL_PLL1 = (uint32_t)2
- } ClockSel_Opts;
- 
- // TODO: This needs to be confirmed with the RTL.
- /**
-  * \brief   Describes the memory map of the PRCI clock selector MMIO.
-  * \todo    This needs to be confirmed with the RTL.
-  */
- typedef struct {
-   __IO uint32_t UNCORE;                                 // 0x00
-   __IO uint32_t TILE0;                                  // 0x04
-   __IO uint32_t TILE1;                                  // 0x08
-   __IO uint32_t CLKTAP;                                 // 0x0C
- } ClockSel_Type;
- 
- void set_all_clocks(ClockSel_Type* clksel, ClockSel_Opts clksrc);
- 
- #ifdef __cplusplus
- }
- #endif
- 
- #endif // __HAL_RCC_H__
- 
+#ifndef __HAL_RCC_H__
+#define __HAL_RCC_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "metal.h"
+
+/**
+ * \brief   Clock selection mux options.
+ * \note    Creates an enum named `ClockSel_Opts` for later use.
+ */
+typedef enum {
+  CLKSEL_SLOW = (uint32_t)0,
+  CLKSEL_PLL0 = (uint32_t)1,
+  CLKSEL_PLL1 = (uint32_t)2
+} ClockSel_Opts;
+
+/**
+ * \brief   Describes the memory map of the PRCI clock selector MMIO.
+ * \todo    This needs to be confirmed with the RTL.
+ */
+typedef struct {
+  __IO uint32_t UNCORE;                                 // 0x00
+  __IO uint32_t TILE0;                                  // 0x04
+  __IO uint32_t TILE1;                                  // 0x08
+  __IO uint32_t CLKTAP;                                 // 0x0C
+} ClockSel_Type;
+
+void set_all_clocks(ClockSel_Type* clksel, ClockSel_Opts clksrc);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __HAL_RCC_H__

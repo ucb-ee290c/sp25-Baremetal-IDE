f90bbfdeccf58fddfac399ee81161aec053e8212
Marie-Anne Xu <marieanne.xu@berkeley.edu>
2025-12-02 12:02:25 -0800

    testing access

--- platform/bearly25/chip_config.h
+++ platform/bearly25/chip_config.h
@@ -36,13 +36,26 @@ extern "C" {
 // ================================
 //  MMIO devices
 // ================================
-#define UART_BASE                0x10013000U
+// #define DEBUG_CONTROLLER_BASE   0x00000000U
+// #define ERROR_DEVICE_BASE       0x00003000U
+// #define BOOTSEL_BASE            0x00004000U
+// #define BOOT_SELECT_BASE        0x00004000U
+#define BOOTROM_BASE             0x00010000U
+// #define LIBIF_ROM_BASE          0x00020000U
+// #define LIBIF_RAW_BASE          0x00030000U
 #define RCC_BASE                 0x00100000U
 #define CLINT_BASE               0x02000000U
-#define PLIC_BASE                0x0C000000U
-#define TCM_BASE                 0x78000000U
+// #define CACHE_CONTROLLER_BASE   0x02010000U
 #define SCRATCHPAD_BASE          0x08000000U
+#define PLIC_BASE                0x0C000000U
+#define GPIO_BASE                0x10010000U
+#define UART_BASE                0x10013000U
+#define QSPI_BASE                0x10030000U
+#define I2C_BASE                 0x10040000U
+#define QSPI_FLASH_BASE          0x20000000U
 #define DRAM_BASE                0x80000000U
+// #define TCM_BASE                 0x78000000U
+#define TCM_BASE                 0x08010000U
 
 #define RCC_CLOCK_SELECTOR       ((ClockSel_Type*)(RCC_BASE + 0x30000))
 #define CLINT                    ((CLINT_Type *)CLINT_BASE)
@@ -54,6 +67,15 @@ extern "C" {
 
 #define CONV2D_BASE              0x08808000U
 #define CONV2D                   ((Conv2D_Accel_Type *)CONV2D_BASE)
+
+#define GPIOA_BASE              (GPIO_BASE)
+#define GPIOA                   ((GPIO_Type *)GPIOA_BASE)
+
+#define QSPI0_BASE              (QSPI_BASE)
+#define QSPI0                   ((QSPI_Type *)QSPI0_BASE)
+
+#define I2C0_BASE               (I2C_BASE)
+#define I2C0                    ((I2C_Type *)I2C0_BASE)
 
 
 #ifdef __cplusplus

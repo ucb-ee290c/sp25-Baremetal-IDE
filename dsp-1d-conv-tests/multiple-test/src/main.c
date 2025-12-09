4256557a4bba93454c1a1acabc20200a162d5913
LJ Labata <louiejoshualabata@berkeley.edu>
2025-12-09 13:40:30 -0800

    Deleted test-multiple-test and put into multiple-test it works!!!

--- dsp-1d-conv-tests/multiple-test/src/main.c
+++ dsp-1d-conv-tests/multiple-test/src/main.c
@@ -61,11 +61,59 @@ union Converter {
     uint32_t u;
 };
 
-// static uint64_t read_cycles() {
-//     uint64_t cycles;
-//     asm volatile ("rdcycle %0" : "=r" (cycles));
-//     return cycles;
-// }
+void convolution_1D(uint32_t *arr, size_t arr_len, uint32_t *kernel, size_t kernel_len, size_t dilation, float *output) {
+  
+  /* 
+  Computes the convolution of arr with the given kernel and dilation factor and stores the result in output, specifically 
+  based on the implementation of the convolution block. The first value in the output array is computed with the kernel's 
+  left element aligned with the array's left element.
+
+  arr:        pointer to input array      FP16 array
+  arr_len:    length of input array       
+  kernel:     pointer to kernel array     FP16 array (represented as uint16_t)
+  kernel_len: length of kernel array 
+  dilation:   dilation factor
+  output:     pointer to output array     FP16 array (represented as uint16_t) 
+
+  Example input and output: 
+
+  arr:        {1, 2, 3, 4}
+  arr_len:    4
+  kernel:     {-1, 1, -1}
+  kernel_len: 3
+  dilation:   1
+
+  output: {-2, -3, -1, -4} ({-1*1 + 1*2 + -1*3, -1*2 + 1*3 + -1*4, -1*3 + 1*4 + -1*0, -1*4 + 1*0 + -1*0})
+
+  For border values (at the end), we assume the array is zero-extended to fit the length of the kernel (including dilation).
+  */
+
+  size_t output_len = arr_len + (kernel_len - 1) * dilation;
+
+    for (int i = 0; i < output_len; i++) {
+        output[i] = 0.0f;
+
+        for (int j = 0; j < kernel_len; j++) {
+            int arr_index = i + j * dilation - (kernel_len - 1) * dilation;
+
+            uint32_t item = 0;
+            if (arr_index >= 0 && arr_index < arr_len) {
+                item = arr[arr_index];
+            }
+
+            float float_input = *(float*)&item;
+            float float_kernel = *(float*)&kernel[j]; 
+            output[i] += float_input * float_kernel;
+        }
+    }
+}
+
+void app_main() {
+  uint64_t mhartid = READ_CSR("mhartid");
+
+  printf("Hello world from hart %d: %d\n", mhartid, counter);
+}
+
 
 void first_convolution() {
 
@@ -118,19 +166,19 @@ void first_convolution() {
   // Print the input
   printf("Input (FP32):\n");
   for (int i = 0; i < in_len; i++) {
-    printf("0x%08x ", in_arr[i]);
+    printf("0x%08X ", in_arr[i]);
   }
 
   // Print the kernel
   printf("Kernel (FP32):\n");
   for (int i = 0; i < kernel_len; i++) {
-    printf("0x%08x ", in_kernel[i]);
+    printf("0x%08X ", in_kernel[i]);
   }
 
   // Print the test results
   printf("\nTest Output (FP32):\n");
   for (int i = 0; i < output_len; i++) {
-    printf("0x%08x ", test_out[i]);
+    printf("0x%08X ", test_out[i]);
   }
 
   // Calculate the correct convolution (Golden Model)
@@ -140,7 +188,7 @@ void first_convolution() {
   union Converter converter;
   for (int i = 0; i < output_len; i++) {
       converter.f = ref_out[i];
-      printf("0x%08x ", converter.u);
+      printf("0x%08X ", converter.u);
   }
 
   printf("\nComparing test to reference.\n");
@@ -154,97 +202,92 @@ void first_convolution() {
   printf("\n\n");
 }
 
-// void second_convolution() {
 
-//   // IO:
-//   uint32_t in_arr[16] = {0x41200000,0x41300000,0x41400000,0x41500000,0x3F800000,0x40000000,
-//                            0x40400000,0x40800000,0x41000000,0x40E00000,0x40C00000,0x40A00000,
-//                            0x40800000,0xBF800000,0xC0000000,0xC0400000};
-//   uint32_t in_kernel[8] = {0x3F800000,0xBF800000,0x3F800000,0,0,0,0,0};
+void second_convolution() {
+
+  // IO:
+  uint32_t in_arr[16] = {0x41200000,0x41300000,0x41400000,0x41500000,0x3F800000,0x40000000,
+                           0x40400000,0x40800000,0x41000000,0x40E00000,0x40C00000,0x40A00000,
+                           0x40800000,0xBF800000,0xC0000000,0xC0400000};
+  uint32_t in_kernel[8] = {0x3F800000,0xBF800000,0x3F800000,0,0,0,0,0};
  
-//   uint32_t in_len = 16;
-//   uint8_t kernel_len = 8;
+  uint32_t in_len = 16;
+  uint8_t kernel_len = 8;
   
-//   // Config:
-//   uint16_t in_dilation = 1;
+  // Config:
+  uint16_t in_dilation = 1;
   
-//   // Create array for output data
-//   uint32_t output_len = in_len+kernel_len+-1;
-//   uint32_t test_out[output_len];
+  // Create array for output data
+  uint32_t output_len = in_len+kernel_len+-1;
+  uint32_t test_out[output_len];
   
-//   size_t n = sizeof(test_out);  // Size of test output in bytes
+  size_t n = sizeof(test_out);  // Size of test output in bytes
 
-//   printf("Starting Convolution\n");
+  printf("Starting Convolution\n");
   
-//   uint8_t status = perform_convolution_1D(in_arr, in_len, in_kernel, kernel_len, test_out, in_dilation);
+  uint8_t status = perform_convolution_1D(in_arr, in_len, in_kernel, kernel_len, test_out, in_dilation);
 
-//   if (status != 0) {
-//     // printf("Convolution Failed: %s (%d)\n", get_register_status_human_readable(), status);
+  if (status != 0) {
+    // printf("Convolution Failed: %s (%d)\n", get_register_status_human_readable(), status);
 
-//     printf("Convolution Failed: %d\n", status);
-//     switch (status) {
-//       case -1:   printf("Invalid Kernel Length. Must be 8 or 16"); break;
-//       case 0x01: printf("BUSY"); break;
-//       case 0x02: printf("COMPL"); break;
-//       case 0x04: printf("ERROR"); break;
-//       case 0x08: printf("INVALID"); break;
-//       case 0x10: printf("INFINITE"); break;
-//       case 0x20: printf("OVERFLOW"); break;
-//       case 0x40: printf("UNDERFLOW"); break;
-//       case 0x80: printf("INEXACT"); break;
-//       default: printf("UNKNOWN STATUS"); break;
-//     }
+    printf("Convolution Failed: %d\n", status);
+    switch (status) {
+      case -1:   printf("Invalid Kernel Length. Must be 8 or 16"); break;
+      case 0x01: printf("BUSY"); break;
+      case 0x02: printf("COMPL"); break;
+      case 0x04: printf("ERROR"); break;
+      case 0x08: printf("INVALID"); break;
+      case 0x10: printf("INFINITE"); break;
+      case 0x20: printf("OVERFLOW"); break;
+      case 0x40: printf("UNDERFLOW"); break;
+      case 0x80: printf("INEXACT"); break;
+      default: printf("UNKNOWN STATUS"); break;
+    }
 
-//     return status;
-//   }
+    return status;
+  }
 
-//   printf("Convolution completed!\n");
+  printf("Convolution completed!\n");
 
-//   // Print the input
-//   printf("Input (FP32):\n");
-//   for (int i = 0; i < in_len; i++) {
-//     printf("0x%08x ", in_arr[i]);
-//   }
+  // Print the input
+  printf("Input (FP32):\n");
+  for (int i = 0; i < in_len; i++) {
+    printf("0x%08X ", in_arr[i]);
+  }
 
-//   // Print the kernel
-//   printf("Kernel (FP32):\n");
-//   for (int i = 0; i < kernel_len; i++) {
-//     printf("0x%08x ", in_kernel[i]);
-//   }
+  // Print the kernel
+  printf("Kernel (FP32):\n");
+  for (int i = 0; i < kernel_len; i++) {
+    printf("0x%08X ", in_kernel[i]);
+  }
 
-//   // Print the test results
-//   printf("\nTest Output (FP32):\n");
-//   for (int i = 0; i < output_len; i++) {
-//     printf("0x%08x ", test_out[i]);
-//   }
+  // Print the test results
+  printf("\nTest Output (FP32):\n");
+  for (int i = 0; i < output_len; i++) {
+    printf("0x%08X ", test_out[i]);
+  }
 
-//   // Calculate the correct convolution (Golden Model)
-//   float ref_out[output_len];
-//   perform_naive_convolution_1D(in_arr, in_len, in_kernel, kernel_len, in_dilation, ref_out);
-//   printf("\nReference Output (FP32):\n");
-//   union Converter converter;
-//   for (int i = 0; i < output_len; i++) {
-//       converter.f = ref_out[i];
-//       printf("0x%08x ", converter.u);
-//   }
+  // Calculate the correct convolution (Golden Model)
+  float ref_out[output_len];
+  perform_naive_convolution_1D(in_arr, in_len, in_kernel, kernel_len, in_dilation, ref_out);
+  printf("\nReference Output (FP32):\n");
+  union Converter converter;
+  for (int i = 0; i < output_len; i++) {
+      converter.f = ref_out[i];
+      printf("0x%08X ", converter.u);
+  }
 
-//   printf("\nComparing test to reference.\n");
+  printf("\nComparing test to reference.\n");
 
-//   // Check if the test passed
-//   if (memcmp(test_out, ref_out, n) == 0) {
-//       printf("[TEST PASSED]: Test Output matches Reference Output.");
-//   } else {
-//       printf("[TEST FAILED]: Test Output does not match Reference Output.");
-//   }
-//   printf("\n\n");
-// }
-
-
-void app_main() {
-  uint64_t mhartid = READ_CSR("mhartid");
-
-  printf("Hello world from hart %d: %d\n", mhartid, counter);
+  // Check if the test passed
+  if (memcmp(test_out, ref_out, n) == 0) {
+      printf("[TEST PASSED]: Test Output matches Reference Output.");
+  } else {
+      printf("[TEST FAILED]: Test Output does not match Reference Output.");
+  }
+  printf("\n\n");
 }
+
 /* USER CODE END PUC */
 
 /**
@@ -252,12 +295,14 @@ void app_main() {
   * @retval int
   */
 int main(int argc, char **argv) {
-  printf('\n');
-  printf("First convolution.\n");
   app_main();
-//   first_convolution();
-  // printf("Second convolution.\n");
-  // second_convolution();
+
+  // printf('\n');  // NOTE: If you uncomment this line your program will hang because
+  printf("\n\nFirst convolution:\n");
+  first_convolution();
+
+  printf("\n\nSecond convolution:\n");
+  second_convolution();
   return 0;
 }
 

213c23b2ea207be7d4a38fd860423157b34d1d72
Yash Kodali <yash.kodali@berkeley.edu>
2026-02-27 06:29:23 -0800

    add pll sweep to conv accelerator bmarks

--- bearly25-bmarks/conv-bmarks/src/main.c
+++ bearly25-bmarks/conv-bmarks/src/main.c
@@ -6,18 +6,51 @@
 
 uint64_t target_frequency = CONV_BENCH_TARGET_FREQUENCY_HZ;
 
+static void run_suite_for_frequency(uint64_t frequency_hz) {
+  printf("\n=== CONV-BMARKS @ %llu Hz ===\n",
+         (unsigned long long)frequency_hz);
+  conv_bench_run_all();
+  printf("=== CONV-BMARKS DONE @ %llu Hz ===\n",
+         (unsigned long long)frequency_hz);
+}
+
 void app_init(void) {
   init_test(target_frequency);
 }
 
 void app_main(void) {
-  conv_bench_run_all();
+  run_suite_for_frequency(target_frequency);
 }
 
+#if CONV_BENCH_ENABLE_PLL_SWEEP
+static const uint64_t k_pll_sweep_freqs_hz[] = {
+  CONV_BENCH_PLL_FREQ_LIST
+};
+#endif
+
 int main(void) {
+#if CONV_BENCH_ENABLE_PLL_SWEEP
+  const size_t num_freqs =
+      sizeof(k_pll_sweep_freqs_hz) / sizeof(k_pll_sweep_freqs_hz[0]);
+  if (num_freqs == 0u) {
+    return 0;
+  }
+
+  target_frequency = k_pll_sweep_freqs_hz[0];
+  init_test(target_frequency);
+  run_suite_for_frequency(target_frequency);
+
+  for (size_t i = 1; i < num_freqs; ++i) {
+    target_frequency = k_pll_sweep_freqs_hz[i];
+    reconfigure_pll(target_frequency, CONV_BENCH_PLL_SWEEP_SLEEP_MS);
+    run_suite_for_frequency(target_frequency);
+  }
+  return 0;
+#else
   app_init();
   app_main();
   return 0;
+#endif
 }
 
 void __attribute__((weak, noreturn)) __main(void) {

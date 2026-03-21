c2c02451cb2853a5929944cae37290b2ce335c5d
Yash Kodali <yash.kodali@berkeley.edu>
2026-03-21 15:52:46 -0700

    make specialized hopefully better than standard this time

--- dsp25-tests/mfcc-test/src/mfcc_specialized_f16.c
+++ dsp25-tests/mfcc-test/src/mfcc_specialized_f16.c
@@ -5,145 +5,75 @@
 #include "dsp/basic_math_functions_f16.h"
 #include "dsp/complex_math_functions_f16.h"
 #include "dsp/fast_math_functions_f16.h"
+#include "dsp/matrix_functions_f16.h"
 #include "dsp/statistics_functions_f16.h"
 #include "dsp/transform_functions_f16.h"
+
+#ifndef MFCC_SPECIALIZED_RUNTIME_CHECK
+#define MFCC_SPECIALIZED_RUNTIME_CHECK 0
+#endif
 
 #ifndef MFCC_F16_ACCUM_MODE
 #define MFCC_F16_ACCUM_MODE 0
 #endif
 
-static void mfcc_tinyspeech_apply_mel_f16(const riscv_mfcc_instance_f16 *S,
-                                          const float16_t *spectrum,
-                                          float16_t *mel_out)
+static __STATIC_FORCEINLINE void mfcc_tinyspeech_apply_mel_f16(const riscv_mfcc_instance_f16 *S,
+                                                                const float16_t *spectrum,
+                                                                float16_t *mel_out)
 {
   const float16_t *coef = S->filterCoefs;
 
   for (uint32_t i = 0; i < MFCC_TINYSPEECH_NUM_MEL; i++) {
-    const float16_t *spec = spectrum + S->filterPos[i];
+    const float16_t *pMel = spectrum + S->filterPos[i];
+    const float16_t *pCoef = coef;
     uint32_t len = S->filterLengths[i];
+    float16_t result;
 #if (MFCC_F16_ACCUM_MODE == 1)
-    float32_t acc0 = 0.0f;
-    float32_t acc1 = 0.0f;
-    float32_t acc2 = 0.0f;
-    float32_t acc3 = 0.0f;
-#else
-    float16_t acc0 = 0.0f16;
-    float16_t acc1 = 0.0f16;
-    float16_t acc2 = 0.0f16;
-    float16_t acc3 = 0.0f16;
-#endif
-    uint32_t n = 0;
-
-    for (; (n + 3U) < len; n += 4U) {
-#if (MFCC_F16_ACCUM_MODE == 1)
-      acc0 += (float32_t)spec[n] * (float32_t)coef[n];
-      acc1 += (float32_t)spec[n + 1U] * (float32_t)coef[n + 1U];
-      acc2 += (float32_t)spec[n + 2U] * (float32_t)coef[n + 2U];
-      acc3 += (float32_t)spec[n + 3U] * (float32_t)coef[n + 3U];
-#else
-      acc0 += (_Float16)spec[n] * (_Float16)coef[n];
-      acc1 += (_Float16)spec[n + 1U] * (_Float16)coef[n + 1U];
-      acc2 += (_Float16)spec[n + 2U] * (_Float16)coef[n + 2U];
-      acc3 += (_Float16)spec[n + 3U] * (_Float16)coef[n + 3U];
-#endif
+    float32_t acc32 = 0.0f;
+    for (uint32_t n = 0; n < len; n++) {
+      acc32 += (float32_t)pMel[n] * (float32_t)pCoef[n];
     }
-
-#if (MFCC_F16_ACCUM_MODE == 1)
-    {
-      float32_t acc = (acc0 + acc1) + (acc2 + acc3);
-      for (; n < len; n++) {
-        acc += (float32_t)spec[n] * (float32_t)coef[n];
-      }
-      mel_out[i] = (float16_t)acc;
-    }
+    result = (float16_t)acc32;
 #else
-    {
-      float16_t acc = (float16_t)((_Float16)(acc0 + acc1) + (_Float16)(acc2 + acc3));
-      for (; n < len; n++) {
-        acc += (_Float16)spec[n] * (_Float16)coef[n];
+  #if defined(RISCV_MATH_VECTOR_F16)
+    if (len <= 16U) {
+      uint32_t n = len;
+      result = 0.0f16;
+      while (n > 0U) {
+        result += (_Float16)(*pMel++) * (_Float16)(*pCoef++);
+        n--;
       }
-      mel_out[i] = acc;
+    } else
+  #endif
+    {
+      riscv_dot_prod_f16(pMel, pCoef, len, &result);
     }
 #endif
 
     coef += len;
+    mel_out[i] = result;
   }
 }
 
-static void mfcc_tinyspeech_apply_dct_f16(const riscv_mfcc_instance_f16 *S,
-                                          const float16_t *mel,
-                                          float16_t *out)
+static __STATIC_FORCEINLINE void mfcc_tinyspeech_apply_dct_f16(const riscv_mfcc_instance_f16 *S,
+                                                                const float16_t *mel,
+                                                                float16_t *out)
 {
-#if defined(RISCV_MATH_VECTOR_F16)
-  uint32_t rows_remaining = MFCC_TINYSPEECH_NUM_DCT;
-  const float16_t *dct_base = S->dctCoefs;
-  float16_t *dst = out;
-  const ptrdiff_t stride = (ptrdiff_t)(MFCC_TINYSPEECH_NUM_MEL * sizeof(float16_t));
-
-  while (rows_remaining > 0U) {
-    size_t l = __riscv_vsetvl_e16m8(rows_remaining);
-    vfloat16m8_t acc = __riscv_vfmv_v_f_f16m8(0.0f, l);
-
-    for (uint32_t c = 0; c < MFCC_TINYSPEECH_NUM_MEL; c++) {
-      vfloat16m8_t vc = __riscv_vlse16_v_f16m8(dct_base + c, stride, l);
-      acc = __riscv_vfmacc_vf_f16m8(acc, mel[c], vc, l);
+#if (MFCC_F16_ACCUM_MODE == 1)
+  for (uint32_t i = 0; i < MFCC_TINYSPEECH_NUM_DCT; i++) {
+    const float16_t *pDct = S->dctCoefs + (i * MFCC_TINYSPEECH_NUM_MEL);
+    float32_t acc32 = 0.0f;
+    for (uint32_t j = 0; j < MFCC_TINYSPEECH_NUM_MEL; j++) {
+      acc32 += (float32_t)pDct[j] * (float32_t)mel[j];
     }
-
-    __riscv_vse16_v_f16m8(dst, acc, l);
-    dst += l;
-    dct_base += l * MFCC_TINYSPEECH_NUM_MEL;
-    rows_remaining -= (uint32_t)l;
+    out[i] = (float16_t)acc32;
   }
 #else
-  const float16_t *dct = S->dctCoefs;
-
-  for (uint32_t row = 0; row < MFCC_TINYSPEECH_NUM_DCT; row++) {
-    const float16_t *d = dct + (row * MFCC_TINYSPEECH_NUM_MEL);
-#if (MFCC_F16_ACCUM_MODE == 1)
-    float32_t acc0 = 0.0f;
-    float32_t acc1 = 0.0f;
-    float32_t acc2 = 0.0f;
-    float32_t acc3 = 0.0f;
-#else
-    float16_t acc0 = 0.0f16;
-    float16_t acc1 = 0.0f16;
-    float16_t acc2 = 0.0f16;
-    float16_t acc3 = 0.0f16;
-#endif
-    uint32_t n = 0;
-
-    for (; (n + 3U) < MFCC_TINYSPEECH_NUM_MEL; n += 4U) {
-#if (MFCC_F16_ACCUM_MODE == 1)
-      acc0 += (float32_t)d[n] * (float32_t)mel[n];
-      acc1 += (float32_t)d[n + 1U] * (float32_t)mel[n + 1U];
-      acc2 += (float32_t)d[n + 2U] * (float32_t)mel[n + 2U];
-      acc3 += (float32_t)d[n + 3U] * (float32_t)mel[n + 3U];
-#else
-      acc0 += (_Float16)d[n] * (_Float16)mel[n];
-      acc1 += (_Float16)d[n + 1U] * (_Float16)mel[n + 1U];
-      acc2 += (_Float16)d[n + 2U] * (_Float16)mel[n + 2U];
-      acc3 += (_Float16)d[n + 3U] * (_Float16)mel[n + 3U];
-#endif
-    }
-
-#if (MFCC_F16_ACCUM_MODE == 1)
-    {
-      float32_t acc = (acc0 + acc1) + (acc2 + acc3);
-      for (; n < MFCC_TINYSPEECH_NUM_MEL; n++) {
-        acc += (float32_t)d[n] * (float32_t)mel[n];
-      }
-      out[row] = (float16_t)acc;
-    }
-#else
-    {
-      float16_t acc = (float16_t)((_Float16)(acc0 + acc1) + (_Float16)(acc2 + acc3));
-      for (; n < MFCC_TINYSPEECH_NUM_MEL; n++) {
-        acc += (_Float16)d[n] * (_Float16)mel[n];
-      }
-      out[row] = acc;
-    }
-#endif
-  }
+  riscv_matrix_instance_f16 pDctMat;
+  pDctMat.numRows = MFCC_TINYSPEECH_NUM_DCT;
+  pDctMat.numCols = MFCC_TINYSPEECH_NUM_MEL;
+  pDctMat.pData = (float16_t *)S->dctCoefs;
+  riscv_mat_vec_mult_f16(&pDctMat, mel, out);
 #endif
 }
 
@@ -155,12 +85,14 @@ void mfcc_tinyspeech_256_23_12_f16(const riscv_mfcc_instance_f16 *S,
   float16_t maxValue;
   uint32_t index;
 
+#if (MFCC_SPECIALIZED_RUNTIME_CHECK == 1)
   if ((S->fftLen != MFCC_TINYSPEECH_FFT_LEN) ||
       (S->nbMelFilters != MFCC_TINYSPEECH_NUM_MEL) ||
       (S->nbDctOutputs != MFCC_TINYSPEECH_NUM_DCT)) {
     riscv_mfcc_f16(S, pSrc, pDst, pTmp);
     return;
   }
+#endif
 
   riscv_absmax_f16(pSrc, S->fftLen, &maxValue, &index);
 
--- dsp25-tests/mfcc-test/src/mfcc_specialized_f32.c
+++ dsp25-tests/mfcc-test/src/mfcc_specialized_f32.c
@@ -3,94 +3,38 @@
 #include "dsp/basic_math_functions.h"
 #include "dsp/complex_math_functions.h"
 #include "dsp/fast_math_functions.h"
+#include "dsp/matrix_functions.h"
 #include "dsp/statistics_functions.h"
 #include "dsp/transform_functions.h"
 
-static void mfcc_tinyspeech_apply_mel_f32(const riscv_mfcc_instance_f32 *S,
-                                          const float32_t *spectrum,
-                                          float32_t *mel_out)
+#ifndef MFCC_SPECIALIZED_RUNTIME_CHECK
+#define MFCC_SPECIALIZED_RUNTIME_CHECK 0
+#endif
+
+static __STATIC_FORCEINLINE void mfcc_tinyspeech_apply_mel_f32(const riscv_mfcc_instance_f32 *S,
+                                                                const float32_t *spectrum,
+                                                                float32_t *mel_out)
 {
   const float32_t *coef = S->filterCoefs;
 
   for (uint32_t i = 0; i < MFCC_TINYSPEECH_NUM_MEL; i++) {
-    const float32_t *spec = spectrum + S->filterPos[i];
+    float32_t result;
     uint32_t len = S->filterLengths[i];
-    float32_t acc0 = 0.0f;
-    float32_t acc1 = 0.0f;
-    float32_t acc2 = 0.0f;
-    float32_t acc3 = 0.0f;
-    uint32_t n = 0;
-
-    for (; (n + 3U) < len; n += 4U) {
-      acc0 += spec[n] * coef[n];
-      acc1 += spec[n + 1U] * coef[n + 1U];
-      acc2 += spec[n + 2U] * coef[n + 2U];
-      acc3 += spec[n + 3U] * coef[n + 3U];
-    }
-
-    {
-      float32_t acc = (acc0 + acc1) + (acc2 + acc3);
-      for (; n < len; n++) {
-        acc += spec[n] * coef[n];
-      }
-      mel_out[i] = acc;
-    }
-
+    riscv_dot_prod_f32(spectrum + S->filterPos[i], coef, len, &result);
+    mel_out[i] = result;
     coef += len;
   }
 }
 
-static void mfcc_tinyspeech_apply_dct_f32(const riscv_mfcc_instance_f32 *S,
-                                          const float32_t *mel,
-                                          float32_t *out)
+static __STATIC_FORCEINLINE void mfcc_tinyspeech_apply_dct_f32(const riscv_mfcc_instance_f32 *S,
+                                                                const float32_t *mel,
+                                                                float32_t *out)
 {
-#if defined(RISCV_MATH_VECTOR)
-  uint32_t rows_remaining = MFCC_TINYSPEECH_NUM_DCT;
-  const float32_t *dct_base = S->dctCoefs;
-  float32_t *dst = out;
-  const ptrdiff_t stride = (ptrdiff_t)(MFCC_TINYSPEECH_NUM_MEL * sizeof(float32_t));
-
-  while (rows_remaining > 0U) {
-    size_t l = __riscv_vsetvl_e32m8(rows_remaining);
-    vfloat32m8_t acc = __riscv_vfmv_v_f_f32m8(0.0f, l);
-
-    for (uint32_t c = 0; c < MFCC_TINYSPEECH_NUM_MEL; c++) {
-      vfloat32m8_t vc = __riscv_vlse32_v_f32m8(dct_base + c, stride, l);
-      acc = __riscv_vfmacc_vf_f32m8(acc, mel[c], vc, l);
-    }
-
-    __riscv_vse32_v_f32m8(dst, acc, l);
-    dst += l;
-    dct_base += l * MFCC_TINYSPEECH_NUM_MEL;
-    rows_remaining -= (uint32_t)l;
-  }
-#else
-  const float32_t *dct = S->dctCoefs;
-
-  for (uint32_t row = 0; row < MFCC_TINYSPEECH_NUM_DCT; row++) {
-    const float32_t *d = dct + (row * MFCC_TINYSPEECH_NUM_MEL);
-    float32_t acc0 = 0.0f;
-    float32_t acc1 = 0.0f;
-    float32_t acc2 = 0.0f;
-    float32_t acc3 = 0.0f;
-    uint32_t n = 0;
-
-    for (; (n + 3U) < MFCC_TINYSPEECH_NUM_MEL; n += 4U) {
-      acc0 += d[n] * mel[n];
-      acc1 += d[n + 1U] * mel[n + 1U];
-      acc2 += d[n + 2U] * mel[n + 2U];
-      acc3 += d[n + 3U] * mel[n + 3U];
-    }
-
-    {
-      float32_t acc = (acc0 + acc1) + (acc2 + acc3);
-      for (; n < MFCC_TINYSPEECH_NUM_MEL; n++) {
-        acc += d[n] * mel[n];
-      }
-      out[row] = acc;
-    }
-  }
-#endif
+  riscv_matrix_instance_f32 pDctMat;
+  pDctMat.numRows = MFCC_TINYSPEECH_NUM_DCT;
+  pDctMat.numCols = MFCC_TINYSPEECH_NUM_MEL;
+  pDctMat.pData = (float32_t *)S->dctCoefs;
+  riscv_mat_vec_mult_f32(&pDctMat, mel, out);
 }
 
 void mfcc_tinyspeech_256_23_12_f32(const riscv_mfcc_instance_f32 *S,
@@ -101,12 +45,14 @@ void mfcc_tinyspeech_256_23_12_f32(const riscv_mfcc_instance_f32 *S,
   float32_t maxValue;
   uint32_t index;
 
+#if (MFCC_SPECIALIZED_RUNTIME_CHECK == 1)
   if ((S->fftLen != MFCC_TINYSPEECH_FFT_LEN) ||
       (S->nbMelFilters != MFCC_TINYSPEECH_NUM_MEL) ||
       (S->nbDctOutputs != MFCC_TINYSPEECH_NUM_DCT)) {
     riscv_mfcc_f32(S, pSrc, pDst, pTmp);
     return;
   }
+#endif
 
   riscv_absmax_f32(pSrc, S->fftLen, &maxValue, &index);
 

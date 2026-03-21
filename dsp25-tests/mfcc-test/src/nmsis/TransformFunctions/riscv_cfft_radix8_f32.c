da5b0e5c25d7b0c2e4a730c9f513fd5376e90a70
Yash Kodali <yash.kodali@berkeley.edu>
2026-03-21 15:05:40 -0700

    added vectorized butterfly execution

--- dsp25-tests/mfcc-test/src/nmsis/TransformFunctions/riscv_cfft_radix8_f32.c
+++ dsp25-tests/mfcc-test/src/nmsis/TransformFunctions/riscv_cfft_radix8_f32.c
@@ -47,6 +47,132 @@ RISCV_DSP_ATTRIBUTE void riscv_radix8_butterfly_f32(
       n2 = n2 >> 3;
       i1 = 0;
 
+#if defined(RISCV_MATH_VECTOR)
+      {
+         uint32_t blkCnt = fftLen / n1;
+         size_t vl;
+         ptrdiff_t stride = (ptrdiff_t)(2U * n1 * sizeof(float32_t));
+         float32_t *pI1 = pSrc;
+         float32_t *pI2 = pSrc + (2U * n2);
+         float32_t *pI3 = pSrc + (4U * n2);
+         float32_t *pI4 = pSrc + (6U * n2);
+         float32_t *pI5 = pSrc + (8U * n2);
+         float32_t *pI6 = pSrc + (10U * n2);
+         float32_t *pI7 = pSrc + (12U * n2);
+         float32_t *pI8 = pSrc + (14U * n2);
+
+         while ((vl = __riscv_vsetvl_e32m8(blkCnt)) > 0)
+         {
+            vfloat32m8_t v1r = __riscv_vlse32_v_f32m8(pI1, stride, vl);
+            vfloat32m8_t v1i = __riscv_vlse32_v_f32m8(pI1 + 1, stride, vl);
+            vfloat32m8_t v2r = __riscv_vlse32_v_f32m8(pI2, stride, vl);
+            vfloat32m8_t v2i = __riscv_vlse32_v_f32m8(pI2 + 1, stride, vl);
+            vfloat32m8_t v3r = __riscv_vlse32_v_f32m8(pI3, stride, vl);
+            vfloat32m8_t v3i = __riscv_vlse32_v_f32m8(pI3 + 1, stride, vl);
+            vfloat32m8_t v4r = __riscv_vlse32_v_f32m8(pI4, stride, vl);
+            vfloat32m8_t v4i = __riscv_vlse32_v_f32m8(pI4 + 1, stride, vl);
+            vfloat32m8_t v5r = __riscv_vlse32_v_f32m8(pI5, stride, vl);
+            vfloat32m8_t v5i = __riscv_vlse32_v_f32m8(pI5 + 1, stride, vl);
+            vfloat32m8_t v6r = __riscv_vlse32_v_f32m8(pI6, stride, vl);
+            vfloat32m8_t v6i = __riscv_vlse32_v_f32m8(pI6 + 1, stride, vl);
+            vfloat32m8_t v7r = __riscv_vlse32_v_f32m8(pI7, stride, vl);
+            vfloat32m8_t v7i = __riscv_vlse32_v_f32m8(pI7 + 1, stride, vl);
+            vfloat32m8_t v8r = __riscv_vlse32_v_f32m8(pI8, stride, vl);
+            vfloat32m8_t v8i = __riscv_vlse32_v_f32m8(pI8 + 1, stride, vl);
+
+            vfloat32m8_t vr1 = __riscv_vfadd_vv_f32m8(v1r, v5r, vl);
+            vfloat32m8_t vr5 = __riscv_vfsub_vv_f32m8(v1r, v5r, vl);
+            vfloat32m8_t vr2 = __riscv_vfadd_vv_f32m8(v2r, v6r, vl);
+            vfloat32m8_t vr6 = __riscv_vfsub_vv_f32m8(v2r, v6r, vl);
+            vfloat32m8_t vr3 = __riscv_vfadd_vv_f32m8(v3r, v7r, vl);
+            vfloat32m8_t vr7 = __riscv_vfsub_vv_f32m8(v3r, v7r, vl);
+            vfloat32m8_t vr4 = __riscv_vfadd_vv_f32m8(v4r, v8r, vl);
+            vfloat32m8_t vr8 = __riscv_vfsub_vv_f32m8(v4r, v8r, vl);
+
+            vfloat32m8_t vt1 = __riscv_vfsub_vv_f32m8(vr1, vr3, vl);
+            vr1 = __riscv_vfadd_vv_f32m8(vr1, vr3, vl);
+            vr3 = __riscv_vfsub_vv_f32m8(vr2, vr4, vl);
+            vr2 = __riscv_vfadd_vv_f32m8(vr2, vr4, vl);
+
+            vfloat32m8_t vOut1r = __riscv_vfadd_vv_f32m8(vr1, vr2, vl);
+            vfloat32m8_t vOut5r = __riscv_vfsub_vv_f32m8(vr1, vr2, vl);
+
+            vfloat32m8_t vs1 = __riscv_vfadd_vv_f32m8(v1i, v5i, vl);
+            vfloat32m8_t vs5 = __riscv_vfsub_vv_f32m8(v1i, v5i, vl);
+            vfloat32m8_t vs2 = __riscv_vfadd_vv_f32m8(v2i, v6i, vl);
+            vfloat32m8_t vs6 = __riscv_vfsub_vv_f32m8(v2i, v6i, vl);
+            vfloat32m8_t vs3 = __riscv_vfadd_vv_f32m8(v3i, v7i, vl);
+            vfloat32m8_t vs7 = __riscv_vfsub_vv_f32m8(v3i, v7i, vl);
+            vfloat32m8_t vs4 = __riscv_vfadd_vv_f32m8(v4i, v8i, vl);
+            vfloat32m8_t vs8 = __riscv_vfsub_vv_f32m8(v4i, v8i, vl);
+
+            vfloat32m8_t vt2 = __riscv_vfsub_vv_f32m8(vs1, vs3, vl);
+            vs1 = __riscv_vfadd_vv_f32m8(vs1, vs3, vl);
+            vs3 = __riscv_vfsub_vv_f32m8(vs2, vs4, vl);
+            vs2 = __riscv_vfadd_vv_f32m8(vs2, vs4, vl);
+
+            vfloat32m8_t vOut1i = __riscv_vfadd_vv_f32m8(vs1, vs2, vl);
+            vfloat32m8_t vOut5i = __riscv_vfsub_vv_f32m8(vs1, vs2, vl);
+            vfloat32m8_t vOut3r = __riscv_vfadd_vv_f32m8(vt1, vs3, vl);
+            vfloat32m8_t vOut7r = __riscv_vfsub_vv_f32m8(vt1, vs3, vl);
+            vfloat32m8_t vOut3i = __riscv_vfsub_vv_f32m8(vt2, vr3, vl);
+            vfloat32m8_t vOut7i = __riscv_vfadd_vv_f32m8(vt2, vr3, vl);
+
+            vfloat32m8_t vTmpR1 = __riscv_vfmul_vf_f32m8(__riscv_vfsub_vv_f32m8(vr6, vr8, vl), C81, vl);
+            vr6 = __riscv_vfmul_vf_f32m8(__riscv_vfadd_vv_f32m8(vr6, vr8, vl), C81, vl);
+            vfloat32m8_t vTmpI1 = __riscv_vfmul_vf_f32m8(__riscv_vfsub_vv_f32m8(vs6, vs8, vl), C81, vl);
+            vs6 = __riscv_vfmul_vf_f32m8(__riscv_vfadd_vv_f32m8(vs6, vs8, vl), C81, vl);
+
+            vfloat32m8_t vTmpR2 = __riscv_vfsub_vv_f32m8(vr5, vTmpR1, vl);
+            vr5 = __riscv_vfadd_vv_f32m8(vr5, vTmpR1, vl);
+            vr8 = __riscv_vfsub_vv_f32m8(vr7, vr6, vl);
+            vr7 = __riscv_vfadd_vv_f32m8(vr7, vr6, vl);
+            vfloat32m8_t vTmpI2 = __riscv_vfsub_vv_f32m8(vs5, vTmpI1, vl);
+            vs5 = __riscv_vfadd_vv_f32m8(vs5, vTmpI1, vl);
+            vs8 = __riscv_vfsub_vv_f32m8(vs7, vs6, vl);
+            vs7 = __riscv_vfadd_vv_f32m8(vs7, vs6, vl);
+
+            vfloat32m8_t vOut2r = __riscv_vfadd_vv_f32m8(vr5, vs7, vl);
+            vfloat32m8_t vOut8r = __riscv_vfsub_vv_f32m8(vr5, vs7, vl);
+            vfloat32m8_t vOut6r = __riscv_vfadd_vv_f32m8(vTmpR2, vs8, vl);
+            vfloat32m8_t vOut4r = __riscv_vfsub_vv_f32m8(vTmpR2, vs8, vl);
+            vfloat32m8_t vOut2i = __riscv_vfsub_vv_f32m8(vs5, vr7, vl);
+            vfloat32m8_t vOut8i = __riscv_vfadd_vv_f32m8(vs5, vr7, vl);
+            vfloat32m8_t vOut6i = __riscv_vfsub_vv_f32m8(vTmpI2, vr8, vl);
+            vfloat32m8_t vOut4i = __riscv_vfadd_vv_f32m8(vTmpI2, vr8, vl);
+
+            __riscv_vsse32_v_f32m8(pI1, stride, vOut1r, vl);
+            __riscv_vsse32_v_f32m8(pI1 + 1, stride, vOut1i, vl);
+            __riscv_vsse32_v_f32m8(pI2, stride, vOut2r, vl);
+            __riscv_vsse32_v_f32m8(pI2 + 1, stride, vOut2i, vl);
+            __riscv_vsse32_v_f32m8(pI3, stride, vOut3r, vl);
+            __riscv_vsse32_v_f32m8(pI3 + 1, stride, vOut3i, vl);
+            __riscv_vsse32_v_f32m8(pI4, stride, vOut4r, vl);
+            __riscv_vsse32_v_f32m8(pI4 + 1, stride, vOut4i, vl);
+            __riscv_vsse32_v_f32m8(pI5, stride, vOut5r, vl);
+            __riscv_vsse32_v_f32m8(pI5 + 1, stride, vOut5i, vl);
+            __riscv_vsse32_v_f32m8(pI6, stride, vOut6r, vl);
+            __riscv_vsse32_v_f32m8(pI6 + 1, stride, vOut6i, vl);
+            __riscv_vsse32_v_f32m8(pI7, stride, vOut7r, vl);
+            __riscv_vsse32_v_f32m8(pI7 + 1, stride, vOut7i, vl);
+            __riscv_vsse32_v_f32m8(pI8, stride, vOut8r, vl);
+            __riscv_vsse32_v_f32m8(pI8 + 1, stride, vOut8i, vl);
+
+            {
+               uint32_t elemInc = (uint32_t)(2U * n1 * (uint32_t)vl);
+               pI1 += elemInc;
+               pI2 += elemInc;
+               pI3 += elemInc;
+               pI4 += elemInc;
+               pI5 += elemInc;
+               pI6 += elemInc;
+               pI7 += elemInc;
+               pI8 += elemInc;
+            }
+            blkCnt -= (uint32_t)vl;
+         }
+      }
+#else
       do
       {
          i2 = i1 + n2;
@@ -111,6 +237,7 @@ RISCV_DSP_ATTRIBUTE void riscv_radix8_butterfly_f32(
 
          i1 += n1;
       } while (i1 < fftLen);
+#endif
 
       if (n2 < 8)
          break;
@@ -147,6 +274,176 @@ RISCV_DSP_ATTRIBUTE void riscv_radix8_butterfly_f32(
 
          i1 = j;
 
+#if defined(RISCV_MATH_VECTOR)
+         {
+            uint32_t blkCnt = (fftLen - j + n1 - 1U) / n1;
+            size_t vl;
+            ptrdiff_t stride = (ptrdiff_t)(2U * n1 * sizeof(float32_t));
+            float32_t *pI1 = pSrc + (2U * j);
+            float32_t *pI2 = pI1 + (2U * n2);
+            float32_t *pI3 = pI2 + (2U * n2);
+            float32_t *pI4 = pI3 + (2U * n2);
+            float32_t *pI5 = pI4 + (2U * n2);
+            float32_t *pI6 = pI5 + (2U * n2);
+            float32_t *pI7 = pI6 + (2U * n2);
+            float32_t *pI8 = pI7 + (2U * n2);
+
+            while ((vl = __riscv_vsetvl_e32m8(blkCnt)) > 0)
+            {
+               vfloat32m8_t v1r = __riscv_vlse32_v_f32m8(pI1, stride, vl);
+               vfloat32m8_t v1i = __riscv_vlse32_v_f32m8(pI1 + 1, stride, vl);
+               vfloat32m8_t v2r = __riscv_vlse32_v_f32m8(pI2, stride, vl);
+               vfloat32m8_t v2i = __riscv_vlse32_v_f32m8(pI2 + 1, stride, vl);
+               vfloat32m8_t v3r = __riscv_vlse32_v_f32m8(pI3, stride, vl);
+               vfloat32m8_t v3i = __riscv_vlse32_v_f32m8(pI3 + 1, stride, vl);
+               vfloat32m8_t v4r = __riscv_vlse32_v_f32m8(pI4, stride, vl);
+               vfloat32m8_t v4i = __riscv_vlse32_v_f32m8(pI4 + 1, stride, vl);
+               vfloat32m8_t v5r = __riscv_vlse32_v_f32m8(pI5, stride, vl);
+               vfloat32m8_t v5i = __riscv_vlse32_v_f32m8(pI5 + 1, stride, vl);
+               vfloat32m8_t v6r = __riscv_vlse32_v_f32m8(pI6, stride, vl);
+               vfloat32m8_t v6i = __riscv_vlse32_v_f32m8(pI6 + 1, stride, vl);
+               vfloat32m8_t v7r = __riscv_vlse32_v_f32m8(pI7, stride, vl);
+               vfloat32m8_t v7i = __riscv_vlse32_v_f32m8(pI7 + 1, stride, vl);
+               vfloat32m8_t v8r = __riscv_vlse32_v_f32m8(pI8, stride, vl);
+               vfloat32m8_t v8i = __riscv_vlse32_v_f32m8(pI8 + 1, stride, vl);
+
+               vfloat32m8_t vr1 = __riscv_vfadd_vv_f32m8(v1r, v5r, vl);
+               vfloat32m8_t vr5 = __riscv_vfsub_vv_f32m8(v1r, v5r, vl);
+               vfloat32m8_t vr2 = __riscv_vfadd_vv_f32m8(v2r, v6r, vl);
+               vfloat32m8_t vr6 = __riscv_vfsub_vv_f32m8(v2r, v6r, vl);
+               vfloat32m8_t vr3 = __riscv_vfadd_vv_f32m8(v3r, v7r, vl);
+               vfloat32m8_t vr7 = __riscv_vfsub_vv_f32m8(v3r, v7r, vl);
+               vfloat32m8_t vr4 = __riscv_vfadd_vv_f32m8(v4r, v8r, vl);
+               vfloat32m8_t vr8 = __riscv_vfsub_vv_f32m8(v4r, v8r, vl);
+
+               vfloat32m8_t vt1 = __riscv_vfsub_vv_f32m8(vr1, vr3, vl);
+               vr1 = __riscv_vfadd_vv_f32m8(vr1, vr3, vl);
+               vr3 = __riscv_vfsub_vv_f32m8(vr2, vr4, vl);
+               vr2 = __riscv_vfadd_vv_f32m8(vr2, vr4, vl);
+
+               vfloat32m8_t vOut1r = __riscv_vfadd_vv_f32m8(vr1, vr2, vl);
+               vfloat32m8_t vR2 = __riscv_vfsub_vv_f32m8(vr1, vr2, vl);
+
+               vfloat32m8_t vs1 = __riscv_vfadd_vv_f32m8(v1i, v5i, vl);
+               vfloat32m8_t vs5 = __riscv_vfsub_vv_f32m8(v1i, v5i, vl);
+               vfloat32m8_t vs2 = __riscv_vfadd_vv_f32m8(v2i, v6i, vl);
+               vfloat32m8_t vs6 = __riscv_vfsub_vv_f32m8(v2i, v6i, vl);
+               vfloat32m8_t vs3 = __riscv_vfadd_vv_f32m8(v3i, v7i, vl);
+               vfloat32m8_t vs7 = __riscv_vfsub_vv_f32m8(v3i, v7i, vl);
+               vfloat32m8_t vs4 = __riscv_vfadd_vv_f32m8(v4i, v8i, vl);
+               vfloat32m8_t vs8 = __riscv_vfsub_vv_f32m8(v4i, v8i, vl);
+
+               vfloat32m8_t vt2 = __riscv_vfsub_vv_f32m8(vs1, vs3, vl);
+               vs1 = __riscv_vfadd_vv_f32m8(vs1, vs3, vl);
+               vs3 = __riscv_vfsub_vv_f32m8(vs2, vs4, vl);
+               vs2 = __riscv_vfadd_vv_f32m8(vs2, vs4, vl);
+
+               vfloat32m8_t vR1 = __riscv_vfadd_vv_f32m8(vt1, vs3, vl);
+               vfloat32m8_t vT1 = __riscv_vfsub_vv_f32m8(vt1, vs3, vl);
+               vfloat32m8_t vOut1i = __riscv_vfadd_vv_f32m8(vs1, vs2, vl);
+               vfloat32m8_t vS2 = __riscv_vfsub_vv_f32m8(vs1, vs2, vl);
+               vfloat32m8_t vS1 = __riscv_vfsub_vv_f32m8(vt2, vr3, vl);
+               vfloat32m8_t vT2 = __riscv_vfadd_vv_f32m8(vt2, vr3, vl);
+
+               vfloat32m8_t vOut5r = __riscv_vfadd_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vR2, co5, vl),
+                   __riscv_vfmul_vf_f32m8(vS2, si5, vl), vl);
+               vfloat32m8_t vOut5i = __riscv_vfsub_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vS2, co5, vl),
+                   __riscv_vfmul_vf_f32m8(vR2, si5, vl), vl);
+               vfloat32m8_t vOut3r = __riscv_vfadd_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vR1, co3, vl),
+                   __riscv_vfmul_vf_f32m8(vS1, si3, vl), vl);
+               vfloat32m8_t vOut3i = __riscv_vfsub_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vS1, co3, vl),
+                   __riscv_vfmul_vf_f32m8(vR1, si3, vl), vl);
+               vfloat32m8_t vOut7r = __riscv_vfadd_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vT1, co7, vl),
+                   __riscv_vfmul_vf_f32m8(vT2, si7, vl), vl);
+               vfloat32m8_t vOut7i = __riscv_vfsub_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vT2, co7, vl),
+                   __riscv_vfmul_vf_f32m8(vT1, si7, vl), vl);
+
+               vfloat32m8_t vR1b = __riscv_vfmul_vf_f32m8(__riscv_vfsub_vv_f32m8(vr6, vr8, vl), C81, vl);
+               vr6 = __riscv_vfmul_vf_f32m8(__riscv_vfadd_vv_f32m8(vr6, vr8, vl), C81, vl);
+               vfloat32m8_t vS1b = __riscv_vfmul_vf_f32m8(__riscv_vfsub_vv_f32m8(vs6, vs8, vl), C81, vl);
+               vs6 = __riscv_vfmul_vf_f32m8(__riscv_vfadd_vv_f32m8(vs6, vs8, vl), C81, vl);
+
+               vfloat32m8_t vT1b = __riscv_vfsub_vv_f32m8(vr5, vR1b, vl);
+               vr5 = __riscv_vfadd_vv_f32m8(vr5, vR1b, vl);
+               vr8 = __riscv_vfsub_vv_f32m8(vr7, vr6, vl);
+               vr7 = __riscv_vfadd_vv_f32m8(vr7, vr6, vl);
+               vfloat32m8_t vT2b = __riscv_vfsub_vv_f32m8(vs5, vS1b, vl);
+               vs5 = __riscv_vfadd_vv_f32m8(vs5, vS1b, vl);
+               vs8 = __riscv_vfsub_vv_f32m8(vs7, vs6, vl);
+               vs7 = __riscv_vfadd_vv_f32m8(vs7, vs6, vl);
+
+               vfloat32m8_t vR1c = __riscv_vfadd_vv_f32m8(vr5, vs7, vl);
+               vfloat32m8_t vR5c = __riscv_vfsub_vv_f32m8(vr5, vs7, vl);
+               vfloat32m8_t vR6c = __riscv_vfadd_vv_f32m8(vT1b, vs8, vl);
+               vfloat32m8_t vT1c = __riscv_vfsub_vv_f32m8(vT1b, vs8, vl);
+               vfloat32m8_t vS1c = __riscv_vfsub_vv_f32m8(vs5, vr7, vl);
+               vfloat32m8_t vS5c = __riscv_vfadd_vv_f32m8(vs5, vr7, vl);
+               vfloat32m8_t vS6c = __riscv_vfsub_vv_f32m8(vT2b, vr8, vl);
+               vfloat32m8_t vT2c = __riscv_vfadd_vv_f32m8(vT2b, vr8, vl);
+
+               vfloat32m8_t vOut2r = __riscv_vfadd_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vR1c, co2, vl),
+                   __riscv_vfmul_vf_f32m8(vS1c, si2, vl), vl);
+               vfloat32m8_t vOut2i = __riscv_vfsub_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vS1c, co2, vl),
+                   __riscv_vfmul_vf_f32m8(vR1c, si2, vl), vl);
+               vfloat32m8_t vOut8r = __riscv_vfadd_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vR5c, co8, vl),
+                   __riscv_vfmul_vf_f32m8(vS5c, si8, vl), vl);
+               vfloat32m8_t vOut8i = __riscv_vfsub_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vS5c, co8, vl),
+                   __riscv_vfmul_vf_f32m8(vR5c, si8, vl), vl);
+               vfloat32m8_t vOut6r = __riscv_vfadd_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vR6c, co6, vl),
+                   __riscv_vfmul_vf_f32m8(vS6c, si6, vl), vl);
+               vfloat32m8_t vOut6i = __riscv_vfsub_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vS6c, co6, vl),
+                   __riscv_vfmul_vf_f32m8(vR6c, si6, vl), vl);
+               vfloat32m8_t vOut4r = __riscv_vfadd_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vT1c, co4, vl),
+                   __riscv_vfmul_vf_f32m8(vT2c, si4, vl), vl);
+               vfloat32m8_t vOut4i = __riscv_vfsub_vv_f32m8(
+                   __riscv_vfmul_vf_f32m8(vT2c, co4, vl),
+                   __riscv_vfmul_vf_f32m8(vT1c, si4, vl), vl);
+
+               __riscv_vsse32_v_f32m8(pI1, stride, vOut1r, vl);
+               __riscv_vsse32_v_f32m8(pI1 + 1, stride, vOut1i, vl);
+               __riscv_vsse32_v_f32m8(pI2, stride, vOut2r, vl);
+               __riscv_vsse32_v_f32m8(pI2 + 1, stride, vOut2i, vl);
+               __riscv_vsse32_v_f32m8(pI3, stride, vOut3r, vl);
+               __riscv_vsse32_v_f32m8(pI3 + 1, stride, vOut3i, vl);
+               __riscv_vsse32_v_f32m8(pI4, stride, vOut4r, vl);
+               __riscv_vsse32_v_f32m8(pI4 + 1, stride, vOut4i, vl);
+               __riscv_vsse32_v_f32m8(pI5, stride, vOut5r, vl);
+               __riscv_vsse32_v_f32m8(pI5 + 1, stride, vOut5i, vl);
+               __riscv_vsse32_v_f32m8(pI6, stride, vOut6r, vl);
+               __riscv_vsse32_v_f32m8(pI6 + 1, stride, vOut6i, vl);
+               __riscv_vsse32_v_f32m8(pI7, stride, vOut7r, vl);
+               __riscv_vsse32_v_f32m8(pI7 + 1, stride, vOut7i, vl);
+               __riscv_vsse32_v_f32m8(pI8, stride, vOut8r, vl);
+               __riscv_vsse32_v_f32m8(pI8 + 1, stride, vOut8i, vl);
+
+               {
+                  uint32_t elemInc = (uint32_t)(2U * n1 * (uint32_t)vl);
+                  pI1 += elemInc;
+                  pI2 += elemInc;
+                  pI3 += elemInc;
+                  pI4 += elemInc;
+                  pI5 += elemInc;
+                  pI6 += elemInc;
+                  pI7 += elemInc;
+                  pI8 += elemInc;
+               }
+               blkCnt -= (uint32_t)vl;
+            }
+         }
+#else
          do
          {
             /*  index calculation for the input */
@@ -254,6 +551,7 @@ RISCV_DSP_ATTRIBUTE void riscv_radix8_butterfly_f32(
 
             i1 += n1;
          } while (i1 < fftLen);
+#endif
 
          j++;
       } while (j < n2);
